33ea1d0fdb849ccf8eb74e558ca42939036c5031
Alvaro Garcia <derecerca@gmail.com>
2015-08-05 00:25:00 +0200

    refactor: extract function for calculating the multiplicity

--- src/main/clojure/fizzbuzz.clj
+++ src/main/clojure/fizzbuzz.clj
@@ -2,3 +2,15 @@
 
 ;(defn fizzbuzz[n]
 ;	"1")
+
+(defn is-multiple-of? [n divisor]
+  (= 0 (rem n divisor)))
+
+(defn fizzbuzz[n]
+  (map
+	#(cond
+	(is-multiple-of? % 15) "FizzBuzz"
+	(= 0 (rem % 3)) "Fizz"
+	(= 0 (rem % 5)) "Buzz"
+	:else (str %))
+  (range 1 (inc n))))
--- src/test/clojure/fizzbuzz/test.clj
+++ src/test/clojure/fizzbuzz/test.clj
@@ -6,5 +6,15 @@
 (deftest canary-test []
     (is (= 1 1)))
 	
-;(deftest fizz-buzz-1 []
-;	(is (= "1" (fizzbuzz 1))))
+(deftest fizz-buzz-numbers []
+	(is (= '("1") (fizzbuzz 1))))
+	
+(deftest fizz-buzz-up-to-fizz []
+	(is (= '("1" "2" "Fizz") (fizzbuzz 3))))
+	
+(deftest fizz-buzz-up-to-buzz []
+	(is (= '("1" "2" "Fizz" "4" "Buzz") (fizzbuzz 5))))
+	
+(deftest fizz-buzz-up-to-fizzbuzz []
+	(is (= '("1" "2" "Fizz" "4" "Buzz" "Fizz" "7" "8" "Fizz" "Buzz" "11" "Fizz" "13" "14" "FizzBuzz") (fizzbuzz 15))))	
+	
